7136a861af61c404832f9c730df0f6d4af4df15b
Ivan Komarov <ikomarov@kse.org.ua>
2022-12-01 12:45:22 +0200

    added verefication

--- main.py
+++ main.py
@@ -43,6 +43,10 @@ print(f"Your word is '{word}'")
 while not is_game_over():
     guess = input("Your next take: ")
 
+    if guess in guesses:
+        print("You have already used this word")
+        continue
+
     if not guess_is_valid(guess):
         continue
 
@@ -56,3 +60,6 @@ while not is_game_over():
     else:
         errors += 1
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+    if errors == ERRORS_TO_LOSE:
+        print ("You Lost")
+        exit()
